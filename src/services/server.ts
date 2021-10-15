f78c2a9ea7ea53ec95d4f9c77c1b560eebcaeca9
Stefania <stefania.dibacco@gmail.com>
2021-10-14 23:29:07 -0300

    27entregable

--- src/services/server.ts
+++ src/services/server.ts
@@ -1,11 +1,10 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import handlebars from 'express-handlebars';
 import * as http from 'http';
-import routerProductos from '../routes/productos';
+import router from '../routes/index';
 import path from 'path';
-import cookieParser from 'cookie-parser';
 import session from 'express-session';
-import MongoStore from 'connect-mongo';
+import passport from '../middleware/auth';
 
 const app = express();
 
@@ -13,6 +12,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const publicFolderPath = path.resolve(__dirname, '../../public');
 app.use(express.static(publicFolderPath));
+
+app.use(
+  session({
+    secret: 'your secret line of secretness',
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // configuracion de hbs
 const layoutDirPath = path.resolve(__dirname, '../../views/layouts');
@@ -33,70 +43,7 @@ app.engine(
   })
 );
 
-app.use('/api/productos', routerProductos);
-
-app.use(express.json());
-
-const StoreOptions = {
-  store: MongoStore.create({
-    mongoUrl:
-      'mongodb+srv://stefanidb:4diasmas@cluster0.7tqvz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-  }),
-  secret: 'thisismysecrctekey',
-  resave: false,
-  saveUninitialized: false,
-  rolling: true,
-  cookie: { maxAge: 1000 * 60 },
-};
-
-app.use(cookieParser());
-app.use(session(StoreOptions));
-
-// eslint-disable-next-line prefer-const
-let logged = {
-  islogged: false,
-  isTimedOut: false,
-  isDestroyed: false,
-  nombre: '',
-};
-
-app.get('/', (req: any, res) => {
-  console.log('estoy en get');
-  if (req.session) {
-    if (!req.session.nombre && logged.islogged) {
-      logged.islogged = false;
-      logged.isTimedOut = true;
-      res.render('main', logged);
-      logged.isTimedOut = false;
-      logged.nombre = '';
-    }
-  }
-  if (logged.isDestroyed) {
-    res.render('main', logged);
-    logged.nombre = ``;
-    logged.isDestroyed = false;
-  } else {
-    res.render('main', logged);
-  }
-});
-
-app.post('/login', (req: any, res: Response) => {
-  if (req.body.nombre) {
-    const session: any = req.session;
-    session.nombre = req.body.nombre;
-    logged.nombre = req.body.nombre;
-    logged.islogged = true;
-  }
-  res.redirect('/');
-});
-
-app.post('/logout', (req: Request, res: Response) => {
-  const session: any = req.session;
-  session.destroy();
-  logged.islogged = false;
-  logged.isDestroyed = true;
-  res.redirect('/');
-});
+app.use('/api/', router);
 
 // creo mi configuracion para socket
 const myServer = new http.Server(app);
